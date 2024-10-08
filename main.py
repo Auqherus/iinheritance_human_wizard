d2ffc668a6b87cbecbac563a7253aa33809592f7
Auqherus <patrykromanowski@gmail.com>
2024-10-08 20:38:28 +0200

    C: add some examples of class usage

--- main.py
+++ main.py
@@ -17,7 +17,10 @@ class Archer(Hero):
     def __init__(self, name, health, num_arrows):
         super().__init__(name, health)
         self.__num_arrows = num_arrows
-
+    
+    def get_num_arrows(self):
+        return self.__num_arrows
+    
     def shoot(self, target):
         if self.__num_arrows <= 0:
             raise Exception("not enough arrows")
@@ -30,6 +33,9 @@ class Wizard(Hero):
         super().__init__(name, health)
         self.__mana = mana
 
+    def get_mana(self):
+        return self.__mana
+
     def cast(self, target):
         #print(f"mana: {self.__mana}")
         if self.__mana < 25:
@@ -37,3 +43,22 @@ class Wizard(Hero):
         self.__mana -= 25
         target.take_damage(25)
         #print(f"mana after cast: {self.__mana}")
+
+def main():
+
+    hero_1 = Hero("Astarion", 175)
+    archer_1 = Archer("Shadowhearth", 125, 12)
+    wizard_1 = Wizard("Auqherus", 155, 250)
+    print(f"Hero's name: {hero_1.get_name()}")
+    print(f"Hero's health: {hero_1.get_health()}\n")
+
+    print(f"Archers's name: {archer_1.get_name()}")
+    print(f"Archers's health: {archer_1.get_health()}")
+    print(f"Archers's number of arrows: {archer_1.get_num_arrows()}\n")
+
+    print(f"Wizard's name: {wizard_1.get_name()}")
+    print(f"Wizard's health: {wizard_1.get_health()}")
+    print(f"Wizard's mana: {wizard_1.get_mana()}")
+
+
+main()
